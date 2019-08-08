8c85a1e82ffe0c4d218febb33ed7ffcc2d195279
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-08 15:51:08 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,6 @@
 class PigLatinizer
   attr_accessor :text
+<<<<<<< HEAD
   
   # def initialize(text)
   #   @text = text.downcase
@@ -11,6 +12,16 @@ class PigLatinizer
       something+"way"
     else 
       until %w(a e i o u y A E I O U Y).include?(char_array[0])
+=======
+
+  
+  def translate_word(something)
+    char_array = something.downcase.scan /\w/
+    if %w(a e i o u y).include?(char_array[0])
+      something+"way"
+    else 
+      until %w(a e i o u y).include?(char_array[0])
+>>>>>>> 0db6d1a18235bcb4837f60b5a1b9b837aa22d917
         char_array.insert(char_array.count-1, char_array.delete_at(0))
         end
         char_array.join+"ay"
@@ -22,7 +33,11 @@ class PigLatinizer
     word_array = string.split(Regexp.union(delimiters))
     pig_array = []
     word_array.each do |word|
+<<<<<<< HEAD
        pig_array << translate_word(word)
+=======
+       pig_array << translate_word(word.downcase)
+>>>>>>> 0db6d1a18235bcb4837f60b5a1b9b837aa22d917
     end
     pig_array.join(" ")
   end
